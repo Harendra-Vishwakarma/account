73da967abd41b51a49b89f7953c90f758ed0506f
Harendra Vishwakarma <harendra.iiti@gmail.com>
2023-06-21 14:46:49 +0530

    convert from jar to war

--- src/main/java/com/example/account/AccountApplication.java
+++ src/main/java/com/example/account/AccountApplication.java
@@ -2,9 +2,10 @@ package com.example.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AccountApplication {
+public class AccountApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
