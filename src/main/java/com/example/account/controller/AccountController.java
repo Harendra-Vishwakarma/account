5f3c8a39bbb00185a4929e36064e1ac2f5f4283d
Harendra Vishwakarma <harendra.iiti@gmail.com>
2023-06-21 13:39:27 +0530

    change in controller

---
+++ src/main/java/com/example/account/controller/AccountController.java
@@ -0,0 +1,13 @@
+package com.example.account.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+	@GetMapping("/get")
+	public String get() {
+		return "this is rest controller";
+	}
+}
